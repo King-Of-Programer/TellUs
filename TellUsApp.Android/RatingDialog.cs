6dd98a474eb3801e26d097d7a1fdb597446a9f43
King-Of-Programer <alexdichemix@gmail.com>
2024-03-12 14:59:33 +0200

    init

---
+++ TellUsApp.Android/RatingDialog.cs
@@ -0,0 +1,66 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Org.Apache.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TellUsApp.Droid;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(RatingDialog))]
+namespace TellUsApp.Droid
+{
+    public class RatingDialog : IAlert
+    {
+
+        public void ShowRatingDialog()
+        {
+            var activity = MainActivity.mainActivity;
+            var dialogView = activity.LayoutInflater.Inflate(Resource.Layout.rating_dialog, null);
+            var ratingBar = dialogView.FindViewById<RatingBar>(Resource.Id.rating_bar);
+            var dialog = new AlertDialog.Builder(activity)
+                .SetView(dialogView)
+                .SetTitle("Please rate us")
+                .SetPositiveButton("Ok", async (s, e) => {
+                    if (Device.RuntimePlatform == Device.Android)
+                    {
+                        if (ratingBar.Rating > 3)
+                        {
+                            try
+                            {
+                                string url = @"https://play.google.com/";
+                                await Browser.OpenAsync(url, BrowserLaunchMode.External);
+                            }
+                            catch (MethodNotSupportedException ex)
+                            {
+
+                            }
+                        }
+                        else ShowFeedbackDialog();
+                    }
+                })
+                .SetNegativeButton("Cancel", (s, e) => { })
+                .Create();
+
+            dialog.Show();
+        }
+        private void ShowFeedbackDialog()
+        {
+            var dialogView = MainActivity.mainActivity.LayoutInflater.Inflate(Resource.Layout.feedback, null);
+            var dialog = new AlertDialog.Builder(MainActivity.mainActivity)
+               .SetView(dialogView)
+               .SetTitle("Please leave feedback")
+               .SetPositiveButton("Ok", (s, e) => { })
+               .SetNegativeButton("Cancel", (s, e) => { })
+               .Create();
+
+            dialog.Show();
+        }
+    }
+}
---
+++ TellUsApp/TellUsPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TellUsApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TellUsPage : ContentPage
+    {
+        private int _countOfLogin { get; set; } = 8;
+        public TellUsPage()
+        {
+            InitializeComponent();
+            checkCountOfLogin();
+        }
+        public void checkCountOfLogin()
+        {
+            object count = null;
+            App.Current.Properties.TryGetValue("countOfLogin", out count);
+            if (count != null)
+            {
+                _countOfLogin = (int)count;
+            }
+            countOfLogin.Text = _countOfLogin.ToString();
+            if (_countOfLogin == 10)
+            {
+                DependencyService.Get<IAlert>().ShowRatingDialog();
+
+            }
+            _countOfLogin++;
+            App.Current.Properties["countOfLogin"] = _countOfLogin;
+        }
+    }
+}
